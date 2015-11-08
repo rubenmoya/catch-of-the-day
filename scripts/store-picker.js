f6ed3b742333e8a4d2df995a0714ff7bc27939aa
rubenmoya <rmoyarodriguez@gmail.com>
2015-11-08 12:01:02 +0100

    handle storepicker submit.

--- scripts/store-picker.js
+++ scripts/store-picker.js
@@ -1,13 +1,25 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var Helpers = require('./helpers');
+var History = require("react-router").History;
 
 var StorePicker = React.createClass({
 
+  mixins: [History],
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var storeId = this.refs.storeId.value;
+
+    this.history.pushState(null, '/store/' + storeId);
+  },
+
   render: function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.handleSubmit}>
         <h2>Please enter a store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" defaultValue={Helpers.getFunName()} required />
         <input type="submit" />
       </form>
     );
